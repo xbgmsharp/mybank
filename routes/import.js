b09309176d800ac4f62fef94e1bd75421b68e8c4
root <xbgmsharp@gmail.com>
2015-04-14 15:26:39 +0000

    Add support for ING (XLS format) and Halifax and CreditAgricole

--- routes/import.js
+++ routes/import.js
@@ -8,6 +8,7 @@ var multiparty = require('multiparty')
   , util = require('util')
 var moment = require('moment');
 var iconv = require('iconv-lite');
+var halifaxstream = require('../libs/halifaxstream.js');
 
 /* GET import page. */
 router.get('/', function(req, res) {
@@ -18,12 +19,12 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
 
         // Mysql Connect
-        var connection = mysql.createConnection('mysql://root:admin@localhost/accounts?dateStrings=true');
-        //var connection = mysql.createConnection('mysql://root:kaya21@localhost:3307/comptes?dateStrings=true&stringifyObjects=true');
+        var connection = mysql.createConnection('mysql://root:root@localhost:3306/mybank?dateStrings=true');
 
         connection.connect(function(err) {
           if (err) {
             console.error('error connecting: ' + err.stack);
+            res.render('error', { "message" : "Error connecting to database", "error" : err});
             return;
           }
           console.log('connected as id ' + connection.threadId);
@@ -36,16 +37,18 @@ router.post('/', function(req, res) {
 	               	if (err) throw err;
 	              	console.log('The insert row is: ', rows.insertId);
 		});
-
-		console.log(query.sql);
+		//console.log(query.sql);
 	}
 
 	var form = new multiparty.Form();
 	form.parse(req, function(err, fields, files) {
 		console.log(files);
 
+		if (!files || !files.upfile || !files.upfile[0]) { res.end("END file upload"); return; }
+
 		var stream = fs.createReadStream(files.upfile[0].path).pipe(iconv.decodeStream('latin1'));
 
+		/* ING DIRECT CSV */
 		if (files.upfile[0].originalFilename.match(/movimientos/) && files.upfile[0].originalFilename.match(/.csv/)) { // IF BANK ING
 		var csvStream1 = csv
 		 .fromStream(stream, {delimiter:';', quote:'"', headers: true, headers : ["FECHAOPER", "FECHAVALOR", "Op", "IMPORTE", "SALDO"]})
@@ -56,7 +59,6 @@ router.post('/', function(req, res) {
 					data.Montant = data.IMPORTE.replace(",", ".").replace(" ", "");
 					data.Desc = "";
 					data.Country = "ES";
-					data.Desc = "";
 					if (data.Op.match(/GIGANEWS/)) { data.Desc = "GIGANEWS"; }
 					else if (data.Op.match(/NTT/)) { data.Desc = "Salaire"; }
 					else if (data.Op.match(/COFIBAR/)) { data.Desc = "Loyes ES"; }
@@ -66,15 +68,58 @@ router.post('/', function(req, res) {
 			}
 		 })
 		 .on('error', function(error) {
-			console.log('Error ING '+ error);
+			console.log('Error ING DIRECT '+ error);
 		 })
 		 .on("end", function(){
-		 	console.log("done ING");
-			res.send("received ID recorsds", {'Content-Type': 'text/plain'}, 200);
+			console.log("done ING DIRECT");
+			//res.write("received INGDIRECT records");
 		 });
 		}
 
-		if (!files.upfile[0].originalFilename.match(/movimientos/) && files.upfile[0].originalFilename.match(/.csv/)) { // IF BANK SABADELL
+		/* ING DIRECT XLS */
+		if (files.upfile[0].originalFilename.match(/^movimientos/) && files.upfile[0].originalFilename.match(/.xls/)) { // IF BANK ING EXECL Format
+			var XLSX = require('xlsx');
+			var workbook = XLSX.readFile(files.upfile[0].path);
+			var resultcsv = [];
+			workbook.SheetNames.forEach(function(sheetName) {
+				var csv = XLSX.utils.sheet_to_csv(workbook.Sheets[sheetName]);
+				if(csv.length > 0){
+					resultcsv.push("SHEET: " + sheetName);
+					resultcsv.push("");
+					resultcsv.push(csv);
+				}
+				resultcsv.join("\n");
+			});
+			console.log(resultcsv);
+			var csvStream = csv
+			 .fromString(resultcsv[2], {delimiter:',', quote:'"', headers: true, headers : ["FECHAOPER", "FECHAVALOR", "Op", "IMPORTE", "SALDO"]})
+			 .on("record", function(data){
+				if (data.FECHAOPER && data.Op && data.IMPORTE && data.FECHAVALOR && data.SALDO) {
+					if (moment(data.FECHAOPER, 'DD/MM/YYYY').isValid() && moment(data.FECHAVALOR, 'DD/MM/YYYY').isValid()) {
+						data.Date = moment(data.FECHAOPER, 'DD/MM/YYYY').format('YYYY-MM-DD');
+						data.Montant = data.IMPORTE.replace(",", "").replace(" ", "");
+						data.Desc = "";
+						data.Country = "ES";
+						if (data.Op.match(/GIGANEWS/)) { data.Desc = "GIGANEWS"; }
+						else if (data.Op.match(/NTT/)) { data.Desc = "Salaire"; }
+						else if (data.Op.match(/COFIBAR/)) { data.Desc = "Loyes ES"; }
+						console.log(data);
+						dbinsert(data);
+					}
+				}
+			 })
+			 .on('error', function(error) {
+				console.log('Error ING DIRECT XLS '+ error);
+			 })
+			 .on("end", function(){
+				console.log("done ING DIRECT");
+				//res.write("received INGDIRECT XLS records");
+			 });
+		}
+
+		/* SABADELL */
+		var halifax_regex = /^\d*_\d*_\d*\.csv$/;
+		if (!halifax_regex.test(files.upfile[0].originalFilename) && files.upfile[0].originalFilename.match(/.csv/)) { // IF BANK SABADELL
 		var csvStream2 = csv
 //		 .fromStream(stream, {delimiter:';', quote:'"', headers : ["FECHAOPER", "Op", "FECHAVALOR", "IMPORTE", "SALDO", "REFERENCIA 1", "REFERENCIA 2"]})
 		 .fromStream(stream, {delimiter:'|', quote:'"', headers : ["FECHAOPER", "Op", "FECHAVALOR", "IMPORTE", "SALDO", "REFERENCIA 1", "REFERENCIA 2"]})
@@ -99,14 +144,15 @@ router.post('/', function(req, res) {
 			}
 		 })
 		 .on('error', function(error) {
-			console.log('Error SB '+ error);
+			console.log('Error SABADELL '+ error);
 		 })
 		 .on("end", function(){
-		 	console.log("done SB");
-			res.send("received SB recorsds", {'Content-Type': 'text/plain'}, 200);
+			console.log("done SABADELL");
+			//res.write("received SABADELL records");
 		 });
 		}
 
+		/* BNP */
 		if (files.upfile[0].originalFilename.match(/.exl/)) { // IF BANK BNP
 		var csvStream3 = csv
 		 .fromStream(stream, {delimiter:'\t', quote:'"', headers : ["Date", "Op", "Montant"]})
@@ -126,6 +172,7 @@ router.post('/', function(req, res) {
 				else if (data.Op.match(/MR LE DREZEN YANN/)) { data.Desc = "Loyer FR"; }
 				else if (data.Op.match(/^VIREMENT ETRANGER RECU/)) { data.Desc = "Economie Espagne"; }
 				else if (data.Op.match(/^VIRT CPTE A CPTE/)) { data.Desc = "Economie Livret A"; }
+				console.log(data);
 				dbinsert(data);
 			}
 		 })
@@ -133,21 +180,91 @@ router.post('/', function(req, res) {
 			console.log('Error BNP '+ error);
 		 })
 		 .on("end", function(){
-		 	console.log("done BNP");
-			res.send("received BNP recorsds", {'Content-Type': 'text/plain'}, 200);
+			console.log("done BNP");
+			//res.write("received BNP records");
 		 });
 		}
-	
+
+		/* HALIFAX */
+		if (files.upfile[0].originalFilename.match(/.csv/) && halifax_regex.test(files.upfile[0].originalFilename)) { // IF BANK HALIFAX
+                var halifax = new halifaxstream();
+		var stream = fs.createReadStream(files.upfile[0].path).pipe(halifax);
+		var csvStream4 = csv
+		 .fromStream(stream, { delimiter:',', headers: true, headers : ["TransactionDate","TransactionType","SortCode","AccountNumber","TransactionDescription","DebitAmount","CreditAmount","Balance"]})
+		 .on("record", function(data){
+			if (data.TransactionDate && data.TransactionType && data.TransactionDescription && ( data.DebitAmount || data.CreditAmount )) {
+				if (moment(data.TransactionDate, 'DD/MM/YYYY').isValid() ) {
+					data.Date = moment(data.TransactionDate, 'DD/MM/YYYY').format('YYYY-MM-DD');
+					if (data.DebitAmount) {
+						data.Montant = "-" + data.DebitAmount.replace(",", ".");
+					} else if (data.CreditAmount)
+						data.Montant = data.CreditAmount.replace(",", ".");
+					data.Op = data.TransactionType +" "+ data.TransactionDescription;
+					data.Desc = "";
+					data.Country = "UK";
+					if (data.Op.match(/TESCO/)) { data.Desc = "Course"; }
+					else if (data.Op.match(/SAINSBURYS/)) { data.Desc = "Course"; }
+					else if (data.Op.match(/VERIZON/)) { data.Desc = "Salaire"; }
+					console.log(data);
+					dbinsert(data);
+				}
+			}
+		 })
+		 .on('error', function(error) {
+			console.log('Error HALIFAX '+ error);
+		 })
+		 .on("end", function(){
+			console.log("done HALIFAX");
+			//res.write("received HALIFAX records");
+		 });
+		}
+
+		/* Credit Agricole */
+		var CreditAgricole_regex = /^CA\d*_\d*\.CSV$/;
+		if (files.upfile[0].originalFilename.match(/.CSV/) && CreditAgricole_regex.test(files.upfile[0].originalFilename)) { // IF BANK CreditAgricole
+		var csvStream4 = csv
+                 .fromStream(stream, { quote: '"', escape: '"', ignoreEmpty: true, headers: true, strictColumnHandling: false, delimiter:';', headers : ["Date","Libelle","DebitEuros","CreditEuros"]})
+		 .on("record", function(data){
+			if (data.Date && data.Libelle && ( data.DebitEuros || data.CreditEuros )) {
+				if (moment(data.Date, 'DD/MM/YYYY').isValid() ) {
+					data.Date = moment(data.Date, 'DD/MM/YYYY').format('YYYY-MM-DD');
+					if (data.DebitEuros) {
+						data.Montant = "-" + data.DebitEuros.replace(",", ".");
+					} else if (data.CreditEuros)
+						data.Montant = data.CreditEuros.replace(",", ".");
+					data.Op = data.Libelle.replace(/[\n\t\r]/g, "");
+					data.Desc = "";
+					data.Country = "FR";
+					if (data.Op.match(/Assurance Pret/)) { data.Desc = "Pret"; }
+					console.log(data);
+					dbinsert(data);
+				}
+			}
+		 })
+		 .on('error', function(error) {
+			console.log('Error CreditAgricole '+ error);
+		 })
+		 .on("end", function(){
+			console.log("done CreditAgricole");
+			//res.write("received HALIFAX records");
+		 });
+		}
+
+
+		/* Remove the upload file */
 		fs.unlink(files.upfile[0].path, function(err) {
 		        if(err) console.error(err.stack);
 		});
-//		res.writeHead(200, {'content-type': 'text/plain'});
+
+//		res.writeHead(201, {'content-type': 'text/plain'});
 //		res.write('received upload:\n\n');
 //		res.end(util.inspect({fields: fields, files: files}));
+
+		console.log('Upload completed!');
 	});
 
-	//res.send("", {'Content-Type': 'text/plain'}, 200);
-	//res.send();
+//	res.send("import", {'Content-Type': 'text/plain'}, 200);
+//	res.end();
 });
 
 module.exports = router;
