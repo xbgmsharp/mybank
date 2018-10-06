dfbc94ea3a612f3556ac314db099f4b066f547f4
xbgmsharp <flacroix>
2018-10-06 16:58:09 +0200

    Add favicon.ico

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var package = require('./package.json');
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -24,7 +24,7 @@ app.set('title', 'My bank');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
